57475de33cfa8b9c81e7a670b9ce8b7cf5b5b5ed
TheStoneMX@outlook.com <TheStoneMX@outlook.com>
2023-03-03 18:25:25 +0000

    Created Anomalies; Edited AnomaliesComponent.InfoForm and AnomaliesComponent.erythrocyte

---
+++ client_code/Anomalies.py
@@ -0,0 +1,62 @@
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+# This is a module.
+# You can define variables and functions here, and use them from any form. For example, in a top-level form:
+class Article:
+    def __init__(self, title):
+        # search for the row with the given title
+        articles = app_tables.articles.search(q.title == title)
+        # check if a row was found
+        if len(articles) > 0:
+            # get the first row
+            article = articles[0]
+
+            # store the data in instance variables
+            self.title = article['title']
+            self.apariencia = article['Appearance']
+            self.relacion = article['Relevance']
+            self.implicaciones = article['Implications']
+            self.sintomas = article['Symptoms']
+            self.pleomorfica = article['Pleomorphic']
+            self.medica = article['MedPerspective']
+            self.intervenciones = article['Interventions']
+            self.trabajando = article['workingwith']
+            self.investigaciones = article['investigations']
+            self.almica = article['Almica']
+        else:
+            # if no row was found, set all instance variables to None
+            self.title = None
+            self.apariencia = None
+            self.relacion = None
+            self.implicaciones = None
+            self.sintomas = None
+            self.pleomorfica = None
+            self.medica = None
+            self.intervenciones = None
+            self.trabajando = None
+            self.investigaciones = None
+            self.almica = None
+
+    def __str__(self):
+        # define how the object should be represented as a string
+        return f"Article: {self.title}"
+
+    def fill_text_controls(self, apariencia_text_box, relacion_text_box,
+                           implicaciones_text_box, sintomas_text_box,
+                           pleomorfica_text_box, medica_text_box,
+                           intervenciones_text_box, trabajando_text_box,
+                           investigaciones_text_box, almica_text_box):
+        # fill the text controls with the data from the object
+        apariencia_text_box.text = self.apariencia
+        relacion_text_box.text = self.relacion
+        implicaciones_text_box.text = self.implicaciones
+        sintomas_text_box.text = self.sintomas
+        pleomorfica_text_box.text = self.pleomorfica
+        medica_text_box.text = self.medica
+        intervenciones_text_box.text = self.intervenciones
+        trabajando_text_box.text = self.trabajando
+        investigaciones_text_box.text = self.investigaciones
+        almica_text_box.text = self.almica
--- client_code/AnomaliesComponent/erythrocyte/__init__.py
+++ client_code/AnomaliesComponent/erythrocyte/__init__.py
@@ -19,10 +19,115 @@ class erythrocyte(erythrocyteTemplate):
     cmpt.my_string = 'Acanthocytes'
 
   def radio_button_Anisocytosis_clicked(self, **event_args):
-    """This method is called when this radio button is selected"""
-    pass
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Anisocytosis'
+    
+  def radio_button_cloud_patterns_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Cloud Patterns'
+    
+
+  def radio_button_Ghost_Cells_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Ghost Cells'
+
+  def radio_button_Protein_Linkage_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Protein Linkage'
+
+  def radio_button_Schistocytes_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Schistocytes'
+
+  def radio_button_Target_Cells_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Target Cells'
+
+  def radio_button_Erythrocyte_Aggregation_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Erythrocyte Aggregation'
+
+  def radio_button_Single_Membrane_Protrusion_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Single Membrane Protrusion'
+
+  def radio_button_Spherocytes_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Spherocytes'
+
+  def radio_button_Sickle_Cells_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Sickle Cells'
+
+  def radio_button_RBC_Rings_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'RBC Rings'
+
+  def radio_button_Keratocytes_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Keratocytes'
+
+  def radio_button_Echinocytes_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Echinocytes'
+
+  def radio_button_Anisopoikilocytosis_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Anisopoikilocytosis'
+
+  def radio_button_Elliptocytes_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Elliptocytes'
+
+  def radio_button_Poikilocytes_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Poikilocytes'
+
+  def radio_button_Rouleau_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Rouleau'
+
+  def radio_button_Stomatocytes_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Stomatocytes'
+
   def add_component_2_tab_control(self, cmpt):
     # self.link_list_anomalies.role = 'selected'
     self.column_panel_tabcontent.clear()
-    self.column_panel_tabcontent.add_component(cmpt)   
+    self.column_panel_tabcontent.add_component(cmpt) 
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
