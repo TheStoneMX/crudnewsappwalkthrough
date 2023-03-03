64df3182b24fc120b2ed4aae0c3865d92ca3b5fc
TheStoneMX@outlook.com <TheStoneMX@outlook.com>
2023-03-03 16:21:18 +0000

    Created AnomaliesComponent.erythrocyte; Edited AnomaliesComponent.InfoForm; Deleted AnomaliesComponent.InfoForm.erythrocyte

--- client_code/AnomaliesComponent/InfoForm/__init__.py
+++ client_code/AnomaliesComponent/InfoForm/__init__.py
@@ -5,17 +5,19 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+from ..erythrocyte import erythrocyte
 
 class InfoForm(InfoFormTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    self.string = 'erythrocyte'
     # Any code you write here will run before the form opens.
 
   def button_apariencia_click(self, **event_args):
     # we query DB and fill the Info Form
+    cmpt = erythrocyte()
+    
     anvil.server.call('get_desequilibrio', desiquilibrio_name)
 
 
--- client_code/AnomaliesComponent/erythrocyte/__init__.py
+++ client_code/AnomaliesComponent/erythrocyte/__init__.py
@@ -5,15 +5,13 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from InfoForm import InfoForm
 
 class erythrocyte(erythrocyteTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
-
+  
   def radio_button_Acanthocytes_clicked(self, **event_args):
     """This method is called when this radio button is selected"""
     pass
