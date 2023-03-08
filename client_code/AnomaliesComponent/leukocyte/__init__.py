0dab75af88f3cd64324391af15c6909d681e26dc
TheStoneMX@outlook.com <TheStoneMX@outlook.com>
2023-03-08 23:39:47 +0000

    Edited AnomaliesComponent.leukocyte

--- client_code/AnomaliesComponent/leukocyte/__init__.py
+++ client_code/AnomaliesComponent/leukocyte/__init__.py
@@ -6,6 +6,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+from ..InfoForm import InfoForm
+
+
 class leukocyte(leukocyteTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -14,28 +17,117 @@ class leukocyte(leukocyteTemplate):
     # Any code you write here will run before the form opens.
 
   def radio_button_Cohesion_neutrofilos_clicked(self, **event_args):
-    """This method is called when this radio button is selected"""
-    pass
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Cohesion de Neutrofilos'
 
   def radio_button_basofilia_clicked(self, **event_args):
-    """This method is called when this radio button is selected"""
-    pass
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Recuento de basofilos'
 
   def radio_button_Neutrofilos_Hipersegmenados_clicked(self, **event_args):
-    """This method is called when this radio button is selected"""
-    pass
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = ''
 
   def radio_button_Globulos_Blancos_Interrumpidos_clicked(self, **event_args):
-    """This method is called when this radio button is selected"""
-    pass
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'Hipersegmenados'
 
   def radio_button_neutrofilos_no_viables_clicked(self, **event_args):
-    """This method is called when this radio button is selected"""
-    pass
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'no_viables'
 
   def radio_button_neutrofilos_banda_clicked(self, **event_args):
-    """This method is called when this radio button is selected"""
-    pass
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'banda'
+
+  def radio_button_Numero_Aumentado_disminuido_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = ''
+
+  def radio_button_macrocitos_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'macrocitos'
+
+  def radio_button_mielocitos_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'mielocitos'
+
+  def radio_button_metamielocitos_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'metamielocitos'
+
+  def radio_button_empty_wbc_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = ''
+
+  def radio_button_Linfocitos_Plasma_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'empty_wbc'
+
+  def radio_button_wbc_translucent_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = ''
+
+  def radio_button_3_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'translucent'
+
+  def radio_button_interrumpidos_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'interrumpidos'
+
+  def radio_button_eosinofilos_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'eosinofilos'
+
+  def radio_button_basofilos_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'basofilos'
+
+  def radio_button_linfocitos_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'linfocitos'
+
+  def radio_button_monocitos_clicked(self, **event_args):
+    cmpt = InfoForm()
+    self.add_component_2_tab_control(cmpt)
+    cmpt.my_string = 'monocitos'
+
+  def add_component_2_tab_control(self, cmpt):
+    # self.link_list_anomalies.role = 'selected'
+    self.column_panel_tabcontent.clear()
+    self.column_panel_tabcontent.add_component(cmpt)     
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
