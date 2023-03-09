62a4a3b05a8b89111274f24504e8c1698444074a
TheStoneMX@outlook.com <TheStoneMX@outlook.com>
2023-03-09 17:58:35 +0000

    Edited AnomaliesComponent.pleomorphic

--- client_code/AnomaliesComponent/pleomorphic/__init__.py
+++ client_code/AnomaliesComponent/pleomorphic/__init__.py
@@ -12,101 +12,80 @@ class pleomorphic(pleomorphicTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-  def add_component_2_tab_control(self, cmpt):
-    # self.link_list_anomalies.role = 'selected'
-    self.column_panel_tabcontent.clear()
-    self.column_panel_tabcontent.add_component(cmpt)    
-    
-  def radio_button_rbc_paracitados_clicked(self, **event_args):
-    cmpt = InfoForm()
-    self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
-
-  def radio_button_wbc_paracitadas_clicked(self, **event_args):
-    cmpt = InfoForm()
-    self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
-
-  def radio_button_pteroharpen_clicked(self, **event_args):
-    cmpt = InfoForm()
-    self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
-
-  def radio_button_rbc_Fermentados_clicked(self, **event_args):
-    cmpt = InfoForm()
-    self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
-
   def radio_button_ascitis_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
+    cmpt.my_string = 'Ascitis'
 
   def radio_button_protit_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
+    cmpt.my_string = 'Protit'
 
   def radio_button_Sistatogenia_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
+    cmpt.my_string = 'Sistatogenia'
 
   def radio_button_symprotit_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
+    cmpt.my_string = 'Symprotit'
 
   def radio_button_dioekothecits_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
+    cmpt.my_string = 'Dioekothecits'
 
   def radio_button_ascits_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
+    cmpt.my_string = 'Ascits'
 
   def radio_button_talus_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
+    cmpt.my_string = 'Talus'
 
   def radio_button_spermits_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
+    cmpt.my_string = 'Spermits'
 
   def radio_button_PTeroharpen_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
+    cmpt.my_string = 'PTeroharpen'
 
   def radio_button_coloid_tectis_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
+    cmpt.my_string = 'Coloid Tecits'
 
   def radio_button_mychits_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
+    cmpt.my_string = 'Mychits'
 
   def radio_button_condrites_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
+    cmpt.my_string = 'Condrites'
 
   def radio_button_syinascits_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
+    cmpt.my_string = 'Syinascits'
 
   def radio_button_tecits_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
-    cmpt.my_string = ''
+    cmpt.my_string = 'Tecits'
 
+  def add_component_2_tab_control(self, cmpt):
+    # self.link_list_anomalies.role = 'selected'
+    self.column_panel_tabcontent.clear()
+    self.column_panel_tabcontent.add_component(cmpt) 
 
 
 
