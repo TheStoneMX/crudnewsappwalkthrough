c813a777f0d051a15766be1290f0a260221b5a34
TheStoneMX@outlook.com <TheStoneMX@outlook.com>
2024-10-05 12:58:41 +0000

    Edited AnomaliesComponent.InfoForm and AnomaliesComponent.erythrocyte

--- client_code/AnomaliesComponent/erythrocyte/__init__.py
+++ client_code/AnomaliesComponent/erythrocyte/__init__.py
@@ -17,97 +17,116 @@ class erythrocyte(erythrocyteTemplate):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Acanthocytes'
+    cmpt.button_apariencia_click()
 
   def radio_button_Anisocytosis_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Anisocytosis'
+    cmpt.button_apariencia_click()
     
   def radio_button_cloud_patterns_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Cloud Patterns'
+    cmpt.button_apariencia_click()
     
 
   def radio_button_Ghost_Cells_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Ghost Cells'
+    cmpt.button_apariencia_click()
 
   def radio_button_Protein_Linkage_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Protein Linkage'
+    cmpt.button_apariencia_click()
 
   def radio_button_Schistocytes_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Schistocytes'
+    cmpt.button_apariencia_click()
 
   def radio_button_Target_Cells_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Target Cells'
+    cmpt.button_apariencia_click()
 
   def radio_button_Erythrocyte_Aggregation_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Erythrocyte Aggregation'
+    cmpt.button_apariencia_click()
 
   def radio_button_Single_Membrane_Protrusion_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Single Membrane Protrusion'
+    cmpt.button_apariencia_click()
 
   def radio_button_Spherocytes_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Spherocytes'
+    cmpt.button_apariencia_click()
 
   def radio_button_Sickle_Cells_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Sickle Cells'
+    cmpt.button_apariencia_click()
 
   def radio_button_RBC_Rings_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'RBC Rings'
+    cmpt.button_apariencia_click()
 
   def radio_button_Keratocytes_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Keratocytes'
+    cmpt.button_apariencia_click()
 
   def radio_button_Echinocytes_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Echinocytes'
+    cmpt.button_apariencia_click()
 
   def radio_button_Anisopoikilocytosis_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Anisopoikilocytosis'
+    cmpt.button_apariencia_click()
 
   def radio_button_Elliptocytes_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Elliptocytes'
+    cmpt.button_apariencia_click()
 
   def radio_button_Poikilocytes_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Poikilocytes'
+    cmpt.button_apariencia_click()
 
   def radio_button_Rouleau_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Rouleau'
+    cmpt.button_apariencia_click()
 
   def radio_button_Stomatocytes_clicked(self, **event_args):
     cmpt = InfoForm()
     self.add_component_2_tab_control(cmpt)
     cmpt.my_string = 'Stomatocytes'
+    cmpt.button_apariencia_click()
 
   def add_component_2_tab_control(self, cmpt):
     # self.link_list_anomalies.role = 'selected'
